39e64f30bc91055bcfbfd2b31ce4f596ccdaba27
Mystical-Codez <37031914+Mystical-Codez@users.noreply.github.com>
2021-03-05 12:07:23 -0800

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ client.on('message', msg => {
     }
 
     if (msg.content.indexOf("tim ") > -1) {
-        const emoji = bot.emojis.cache.get("817486872749932544");
+        const emoji = client.emojis.cache.get("817486872749932544");
         message.channel.send(`${emoji}`);
     }
     if (msg.channel.id == 516795939768500254) {
