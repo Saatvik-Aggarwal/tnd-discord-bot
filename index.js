00a23137e3151e680e0d1505fe7e01de861f3845
Mystical-Codez <37031914+Mystical-Codez@users.noreply.github.com>
2021-04-07 19:38:05 -0700

    included node fetch package

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const Discord = require('discord.js');
 const request = require('request');
 const WebSocket = require('ws');
+const fetch = require("node-fetch");
 
 // create a new Discord client
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'USER'], fetchAllMembers: true });
