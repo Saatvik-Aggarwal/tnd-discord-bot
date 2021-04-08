3cd64163b8b46b5920048e17f8e0a87914870b11
Mystical-Codez <37031914+Mystical-Codez@users.noreply.github.com>
2021-04-07 20:32:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -486,7 +486,42 @@ client.on('message', msg => {
                 
                 msg.reply(data.answers[0])
             });
-        } else {
+        } } else if (command == "serious") {
+            const url = "https://api.openai.com/v1/answers";
+            // var xhr = new XMLHttpRequest();
+            // xhr.open("POST", url, true);
+            // xhr.setRequestHeader();
+            // xhr.setRequestHeader();
+            // xhr.onload = answerSuccess; 
+            var question = msg.content.split(command);
+            // xhr.send());
+
+            
+              
+
+            fetch(url, {
+                method: "POST",
+                body: JSON.stringify({
+                    "model": "davinci",
+                    "question": question[1].trim(),
+                    "examples": [["What is the distance between the Earth and the sun", "1 AU"]],
+                    "examples_context": "The Sun is 1 AU away from the sun.",
+                    "temperature": 0.1,
+                    "documents": [],
+                    "stop": ["\n", "<|endoftext|>"]
+                }),
+                headers: {"Content-Type": "application/json", "Authorization": "Bearer " + process.env.AI_TOKEN }
+
+            })
+            .then( res => res.json() )
+            .then( data => {
+                console.log(data); 
+                //data = JSON.parse(data);
+                
+                msg.reply(data.answers[0])
+            });
+        }
+         else {
             msg.reply("Unknown command");
         }
 
